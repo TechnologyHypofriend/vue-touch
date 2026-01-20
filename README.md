60c052c7ea041904025358ed50846613c78fb693
pavel@hypofriend.de <pavel@hypofriend.de>
2026-01-20 14:49:29 +0100

    Upgrade vue-touch

--- README.md
+++ README.md
@@ -1,77 +1,191 @@
-# This plugin is deprecated and not maintained anymore.
+# vue-touch (Vue 3)
 
-## vue-touch
+> Touch events plugin for Vue.js 3
 
-> Touch events plugin for Vue.js. **This plugin does not support Vue 2.0 yet.**
-
-This is a directive wrapper for Hammer.js 2.0.
+This is a directive wrapper for Hammer.js 2.0, updated for Vue 3 compatibility.
 
 ## Install
 
-> This branch is only compatible with Vue 1.0. For the Vue 2.0 compatible rewrite, see the `next` branch
+```bash
+npm install vue-touch-vue3 hammerjs
+```
 
-#### CommonJS
+## Setup
 
-- Available through npm as `vue-touch`.
+```js
+import { createApp } from "vue";
+import VueTouch from "vue-touch-vue3";
+import App from "./App.vue";
 
-  ``` js
-  var VueTouch = require('vue-touch')
-  Vue.use(VueTouch)
-  ```
+const app = createApp(App);
+app.use(VueTouch);
+app.mount("#app");
+```
 
 #### Direct include
 
-- You can also directly include it with a `<script>` tag when you have Vue and Hammer.js already included globally. It will automatically install itself, and will add a global `VueTouch`.
+Include Vue, Hammer.js, and vue-touch-vue3.js via `<script>` tags:
+
+```html
+<script src="https://unpkg.com/vue@3"></script>
+<script src="https://unpkg.com/hammerjs@2"></script>
+<script src="vue-touch-vue3.js"></script>
+
+<script>
+  const app = Vue.createApp({
+    /* ... */
+  });
+  app.use(VueTouch);
+  app.mount("#app");
+</script>
+```
 
 ## Usage
 
 #### Using the `v-touch` directive
 
-``` html
-<a v-touch:tap="onTap">Tap me!</a>
+```html
+<template>
+  <a v-touch:tap="onTap">Tap me!</a>
+  <div v-touch:swipeleft="onSwipeLeft">Swipe me!</div>
+</template>
 
-<div v-touch:swipeleft="onSwipeLeft">Swipe me!</div>
+<script setup>
+  function onTap(e) {
+    console.log("Tapped!", e);
+  }
+
+  function onSwipeLeft(e) {
+    console.log("Swiped left!", e);
+  }
+</script>
 ```
+
+#### Available Events
+
+Built-in gesture events:
+
+- `tap`
+- `pan`, `panstart`, `panmove`, `panend`, `panleft`, `panright`, `panup`, `pandown`
+- `pinch`, `pinchstart`, `pinchmove`, `pinchend`, `pinchin`, `pinchout`
+- `press`, `pressup`
+- `rotate`, `rotatestart`, `rotatemove`, `rotateend`
+- `swipe`, `swipeleft`, `swiperight`, `swipeup`, `swipedown`
 
 #### Configuring Recognizer Options
 
-There are two ways to customize recognizer options such as `direction` and `threshold`. The first one is setting global options:
+**Global options:**
 
-``` js
-// change the threshold for all swipe recognizers
+```js
+import VueTouch from "vue-touch-vue3";
+
+// Change the threshold for all swipe recognizers
 VueTouch.config.swipe = {
-  threshold: 200
-}
+  threshold: 200,
+};
+
+app.use(VueTouch);
 ```
 
-Or, you can use the `v-touch-options` directive to configure the behavior on a specific element:
+**Per-element options with `v-touch-options`:**
 
-``` html
+```html
 <!-- detect only horizontal pans with a threshold of 100 -->
-<a
+<div
   v-touch:pan="onPan"
-  v-touch-options:pan="{ direction: 'horizontal', threshold: 100 }">
-</a>
+  v-touch-options:pan="{ direction: 'horizontal', threshold: 100 }"
+>
+  Pan me horizontally
+</div>
 ```
+
+Note: `v-touch-options` must be placed before or alongside `v-touch` on the same element.
 
 #### Registering Custom Events
 
-``` js
-// example registering a custom doubletap event.
-// the `type` indicates the base recognizer to use from Hammer
-// all other options are Hammer recognizer options.
-VueTouch.registerCustomEvent('doubletap', {
-  type: 'tap',
-  taps: 2
-})
-```
-``` html
-<a v-touch:doubletap="onDoubleTap"></a>
+```js
+import VueTouch from "vue-touch-vue3";
+
+// Register a custom doubletap event
+// `type` indicates the base recognizer to use
+VueTouch.registerCustomEvent("doubletap", {
+  type: "tap",
+  taps: 2,
+});
+
+app.use(VueTouch);
 ```
 
-See [Hammer.js documentation](http://hammerjs.github.io/getting-started/) for all available events.
+```html
+<a v-touch:doubletap="onDoubleTap">Double tap me!</a>
+```
 
-See `/example` for a multi-event demo. To build it, run `npm install && npm run build`.
+## Example Component
+
+```vue
+<template>
+  <div class="touch-demo">
+    <div
+      v-touch:pan="onPan"
+      v-touch:tap="onTap"
+      v-touch:swipeleft="onSwipeLeft"
+      v-touch:swiperight="onSwipeRight"
+      v-touch-options:pan="{ direction: 'all' }"
+      class="touch-area"
+      :style="{ transform: `translate(${x}px, ${y}px)` }"
+    >
+      Touch me!
+    </div>
+    <p>Last event: {{ lastEvent }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const x = ref(0);
+const y = ref(0);
+const lastEvent = ref("none");
+
+function onPan(e) {
+  x.value = e.deltaX;
+  y.value = e.deltaY;
+  lastEvent.value = `pan (${e.deltaX}, ${e.deltaY})`;
+}
+
+function onTap() {
+  lastEvent.value = "tap";
+}
+
+function onSwipeLeft() {
+  lastEvent.value = "swipe left";
+}
+
+function onSwipeRight() {
+  lastEvent.value = "swipe right";
+}
+</script>
+
+<style scoped>
+.touch-area {
+  width: 150px;
+  height: 150px;
+  background: #42b883;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: grab;
+  user-select: none;
+}
+</style>
+```
+
+## Migration from Vue 1.x/2.x
+
+The API remains largely the same. Main changes:
+
+1. **Installation**: Use `app.use(VueTouch)` instead of `Vue.use(VueTouch)`
+2. **Import**: Update your import path to the Vue 3 version
 
 ## License
 
--- vue-touch.js
+++ vue-touch.js
@@ -1,126 +1,160 @@
-;(function () {
-
-  var vueTouch = {}
-  var Hammer = typeof require === 'function'
-    ? require('hammerjs')
-    : window.Hammer
-  var gestures = ['tap', 'pan', 'pinch', 'press', 'rotate', 'swipe']
-  var directions = ['up', 'down', 'left', 'right', 'horizontal', 'vertical', 'all']
-  var customEvents = {}
+(function () {
+  var vueTouch = {};
+  var Hammer =
+    typeof require === "function" ? require("hammerjs") : window.Hammer;
+  var gestures = ["tap", "pan", "pinch", "press", "rotate", "swipe"];
+  var directions = [
+    "up",
+    "down",
+    "left",
+    "right",
+    "horizontal",
+    "vertical",
+    "all",
+  ];
+  var customEvents = {};
 
   if (!Hammer) {
-    throw new Error('[vue-touch] cannot locate Hammer.js.')
+    throw new Error("[vue-touch] cannot locate Hammer.js.");
   }
 
   // exposed global options
-  vueTouch.config = {}
+  vueTouch.config = {};
 
-  vueTouch.install = function (Vue) {
-
-    Vue.directive('touch', {
-
-      isFn: true,
-      acceptStatement: true,
-      priority: Vue.directive('on').priority,
-
-      bind: function () {
-        if (!this.el.hammer) {
-          this.el.hammer = new Hammer.Manager(this.el)
+  vueTouch.install = function (app) {
+    app.directive("touch", {
+      mounted: function (el, binding) {
+        if (!el.hammer) {
+          el.hammer = new Hammer.Manager(el);
         }
-        var mc = this.mc = this.el.hammer
-        // determine event type
-        var event = this.arg
+        var mc = el.hammer;
+        var event = binding.arg;
+
         if (!event) {
-          console.warn('[vue-touch] event type argument is required.')
+          console.warn("[vue-touch] event type argument is required.");
+          return;
         }
-        var recognizerType, recognizer
+
+        var recognizerType, recognizer;
 
         if (customEvents[event]) {
           // custom event
-          var custom = customEvents[event]
-          recognizerType = custom.type
-          recognizer = new Hammer[capitalize(recognizerType)](custom)
-          recognizer.recognizeWith(mc.recognizers)
-          mc.add(recognizer)
+          var custom = customEvents[event];
+          recognizerType = custom.type;
+          recognizer = new Hammer[capitalize(recognizerType)](custom);
+          recognizer.recognizeWith(mc.recognizers);
+          mc.add(recognizer);
         } else {
           // built-in event
           for (var i = 0; i < gestures.length; i++) {
             if (event.indexOf(gestures[i]) === 0) {
-              recognizerType = gestures[i]
-              break
+              recognizerType = gestures[i];
+              break;
             }
           }
           if (!recognizerType) {
-            console.warn('[vue-touch] invalid event type: ' + event)
-            return
+            console.warn("[vue-touch] invalid event type: " + event);
+            return;
           }
-          recognizer = mc.get(recognizerType)
+          recognizer = mc.get(recognizerType);
           if (!recognizer) {
-            // add recognizer
-            recognizer = new Hammer[capitalize(recognizerType)]()
-            // make sure multiple recognizers work together...
-            recognizer.recognizeWith(mc.recognizers)
-            mc.add(recognizer)
+            recognizer = new Hammer[capitalize(recognizerType)]();
+            recognizer.recognizeWith(mc.recognizers);
+            mc.add(recognizer);
           }
           // apply global options
-          var globalOptions = vueTouch.config[recognizerType]
+          var globalOptions = vueTouch.config[recognizerType];
           if (globalOptions) {
-            guardDirections(globalOptions)
-            recognizer.set(globalOptions)
+            guardDirections(globalOptions);
+            recognizer.set(globalOptions);
           }
-          // apply local options
+          // apply local options from modifiers or hammerOptions
           var localOptions =
-            this.el.hammerOptions &&
-            this.el.hammerOptions[recognizerType]
+            el.hammerOptions && el.hammerOptions[recognizerType];
           if (localOptions) {
-            guardDirections(localOptions)
-            recognizer.set(localOptions)
+            guardDirections(localOptions);
+            recognizer.set(localOptions);
           }
         }
-        this.recognizer = recognizer
-      },
 
-      update: function (fn) {
-        var mc = this.mc
-        var event = this.arg
-        // teardown old handler
-        if (this.handler) {
-          mc.off(event, this.handler)
-        }
-        if (typeof fn !== 'function') {
-          this.handler = null
+        el._vueTouch = el._vueTouch || {};
+        el._vueTouch[event] = { recognizer: recognizer };
+
+        // set up handler
+        if (typeof binding.value === "function") {
+          el._vueTouch[event].handler = binding.value;
+          mc.on(event, binding.value);
+        } else {
           console.warn(
-            '[vue-touch] invalid handler function for v-touch: ' +
-            this.arg + '="' + this.descriptor.raw
-          )
-        } else {
-          mc.on(event, (this.handler = fn))
+            "[vue-touch] invalid handler function for v-touch:" + event,
+          );
         }
       },
 
-      unbind: function () {
-        if (this.handler) {
-          this.mc.off(this.arg, this.handler)
-        }
-        if (!Object.keys(this.mc.handlers).length) {
-          this.mc.destroy()
-          this.el.hammer = null
-        }
-      }
-    })
+      updated: function (el, binding) {
+        var mc = el.hammer;
+        var event = binding.arg;
 
-    Vue.directive('touch-options', {
-      priority: Vue.directive('on').priority + 1,
-      update: function (options) {
-        var opts = this.el.hammerOptions || (this.el.hammerOptions = {})
-        if (!this.arg) {
-          console.warn('[vue-touch] recognizer type argument for v-touch-options is required.')
-        } else {
-          opts[this.arg] = options
+        if (!mc || !el._vueTouch || !el._vueTouch[event]) return;
+
+        var oldHandler = el._vueTouch[event].handler;
+
+        // teardown old handler
+        if (oldHandler) {
+          mc.off(event, oldHandler);
         }
-      }
-    })
-  }
+
+        if (typeof binding.value === "function") {
+          el._vueTouch[event].handler = binding.value;
+          mc.on(event, binding.value);
+        } else {
+          el._vueTouch[event].handler = null;
+          console.warn(
+            "[vue-touch] invalid handler function for v-touch:" + event,
+          );
+        }
+      },
+
+      unmounted: function (el, binding) {
+        var event = binding.arg;
+        var mc = el.hammer;
+
+        if (!mc || !el._vueTouch || !el._vueTouch[event]) return;
+
+        var handler = el._vueTouch[event].handler;
+        if (handler) {
+          mc.off(event, handler);
+        }
+
+        delete el._vueTouch[event];
+
+        // destroy manager if no handlers left
+        if (!Object.keys(mc.handlers).length) {
+          mc.destroy();
+          el.hammer = null;
+        }
+      },
+    });
+
+    app.directive("touch-options", {
+      created: function (el, binding) {
+        var opts = el.hammerOptions || (el.hammerOptions = {});
+        if (!binding.arg) {
+          console.warn(
+            "[vue-touch] recognizer type argument for v-touch-options is required.",
+          );
+        } else {
+          opts[binding.arg] = binding.value;
+        }
+      },
+      updated: function (el, binding) {
+        var opts = el.hammerOptions || (el.hammerOptions = {});
+        if (binding.arg) {
+          opts[binding.arg] = binding.value;
+        }
+      },
+    });
+  };
 
   /**
    * Register a custom event.
@@ -130,35 +164,38 @@
    *                           required fields:
    *                           - type: the base recognizer to use for this event
    */
-
   vueTouch.registerCustomEvent = function (event, options) {
-    options.event = event
-    customEvents[event] = options
+    options.event = event;
+    customEvents[event] = options;
+  };
+
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  function capitalize (str) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  }
-
-  function guardDirections (options) {
-    var dir = options.direction
-    if (typeof dir === 'string') {
-      var hammerDirection = 'DIRECTION_' + dir.toUpperCase()
-      if (directions.indexOf(dir) > -1 && Hammer.hasOwnProperty(hammerDirection)) {
-        options.direction = Hammer[hammerDirection]
+  function guardDirections(options) {
+    var dir = options.direction;
+    if (typeof dir === "string") {
+      var hammerDirection = "DIRECTION_" + dir.toUpperCase();
+      if (
+        directions.indexOf(dir) > -1 &&
+        Hammer.hasOwnProperty(hammerDirection)
+      ) {
+        options.direction = Hammer[hammerDirection];
       } else {
-        console.warn('[vue-touch] invalid direction: ' + dir)
+        console.warn("[vue-touch] invalid direction: " + dir);
       }
     }
   }
 
-  if (typeof exports == "object") {
-    module.exports = vueTouch
-  } else if (typeof define == "function" && define.amd) {
-    define([], function(){ return vueTouch })
-  } else if (window.Vue) {
-    window.VueTouch = vueTouch
-    Vue.use(vueTouch)
+  // ES Module export
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    module.exports = vueTouch;
+  } else if (typeof define === "function" && define.amd) {
+    define([], function () {
+      return vueTouch;
+    });
+  } else if (typeof window !== "undefined") {
+    window.VueTouch = vueTouch;
   }
-
-})()
+})();
